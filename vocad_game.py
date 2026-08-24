4442780ea732d98ab5f277a6015267aa9d18eebc
414-20-niracha <54603@yupparaj.ac.th>
2026-08-24 14:20:12 +0700

    Update vocad_game.py

--- vocad_game.py
+++ vocad_game.py
@@ -115,8 +115,6 @@ st.session_state.ans2_val = ans2
 st.session_state.ans3_val = ans3
 st.session_state.ans4_val = ans4
 
-
-
 # 4. ปุ่มส่งคำตอบ
 if "start" in st.session_state and not st.session_state.get("is_ended", False):
     if st.button("📥 ส่งคำตอบ"):
