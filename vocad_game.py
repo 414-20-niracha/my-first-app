900023d0f74eb7c72dfb54d32dba65674f438d15
414-20-niracha <54603@yupparaj.ac.th>
2026-08-24 14:30:15 +0700

    Update vocad_game.py

--- vocad_game.py
+++ vocad_game.py
@@ -101,11 +101,11 @@ ans2 = st.text_input(
     value=st.session_state.ans2_val,
 )
 ans3 = st.text_input(
-    "ข้อ 3: Cats love to eat `_ _ n`.🖊️ ",
+    "ข้อ 3: Used to write `_ _ n`.🖊️ ",
     value=st.session_state.ans3_val,
 )
 ans4 = st.text_input(
-    "ข้อ 4: Cats love to eat `_ o c _ _ u _`.🥥  ",
+    "ข้อ 4: On the thees on the beach `_ o c _ _ u _`.🥥  ",
     value=st.session_state.ans4_val,
 
 
